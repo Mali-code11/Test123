b26ddd8e6d795ba282debdcd889bab3e745e989e
Mali-code11 <137727270+Mali-code11@users.noreply.github.com>
2023-12-13 18:51:57 +0100

    Adding Enemy

---
+++ Enemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    public float speed;
+    public Transform target;
+    public int playerDamage = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+       transform.LookAt(target.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player player = other.GetComponent<player>();
+
+        player.TakeDamage(playerDamage);
+    }
+}
